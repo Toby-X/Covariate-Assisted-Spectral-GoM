859c49edae921c2c393b33e8ed54531f00a7bd65
TobyXu <751280216@qq.com>
2023-06-25 15:59:25 +0800

    Finish LCM

--- spectrum_lcm.R
+++ spectrum_lcm.R
@@ -1,10 +1,12 @@
 #-*- coding:utf-8 -*-
 library(Boom)
+library(RSpectra)
+library(Rfast)
 
 ## data generation
 K = 3
 N = 500
-p = N/5
+p = N/50
 r = 10
 Pi = matrix(rep(0,N*K),nrow=N)
 Theta = matrix(rep(0,p*K),ncol=K)
@@ -41,10 +43,54 @@ X = X_t + matrix(rnorm(N*r,0,.1),nrow=N)
 alpha = .1
 
 ## Null
-L.null = A%*%t(A)+
+L.null = mat.mult(A,transpose(A))
+svd.null = svds(L.null,k=K)
+kmeans.null = kmeans(svd.null$u,K,algorithm = "Lloyd", nstart=10,iter.max = 100)
+result.null = apply(Pi,1,which.max)
+for (i in 1:N) {
+  if (result.null[i] == 1){
+    result.null[i] = 2
+  } else if (result.null[i] == 2){
+    result.null[i] = 1
+  }
+}
+mean(kmeans.null$cluster==result.null)
 
 ## Diagonal Deletion
+L.del = Diag.fill(L.null)
+svd.del = eigs(L.del,k=K)
+kmeans.del = kmeans(svd.del$vectors,K,algorithm = "Lloyd", nstart=10,iter.max = 100)
+result.del = apply(Pi,1,which.max)
+mean(kmeans.del$cluster==result.del)
 
 ## Covariate
+L.cov = mat.mult(A,transpose(A))+alpha*mat.mult(X,transpose(X))
+evd.cov = eigs(L.cov,k=K)
+kmeans.cov = kmeans(evd.cov$vectors,K,algorithm = "Lloyd", nstart = 10,iter.max = 100)
+result.cov = apply(Pi,1,which.max)
+for (i in 1:N) {
+  if (result.cov[i] == 2){
+    result.cov[i] = 1
+  } else if (result.cov[i] == 1){
+    result.cov[i] = 3
+  } else{
+    result.cov[i] = 2
+  }
+}
+mean(kmeans.cov$cluster==result.cov)
 
 ## Diagonal Deletion + Covariate
+L.all = Diag.fill(L.cov,0)
+evd.all = eigs(L.all,k=K)
+kmeans.all = kmeans(evd.all$vectors,K,algorithm = "Lloyd", nstart = 10,iter.max = 100)
+result.all = apply(Pi,1,which.max)
+for (i in 1:N) {
+  if (result.all[i] == 2){
+    result.all[i] = 3
+  } else if (result.all[i] == 1){
+    result.all[i] = 2
+  } else{
+    result.all[i] = 1
+  }
+}
+mean(kmeans.all$cluster==result.all)
